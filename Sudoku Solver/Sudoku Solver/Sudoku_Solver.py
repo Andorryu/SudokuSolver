30b1b02edb46815612bc1fbdb00ca3f526986916
Andorryu <adority121@gmail.com>
2022-05-17 18:50:30 -0500

    Started the CheckViable function
    CheckViable function checks to see if every filled tile does not have a repeat in the row

--- Sudoku Solver/Sudoku Solver/Sudoku_Solver.py
+++ Sudoku Solver/Sudoku Solver/Sudoku_Solver.py
@@ -3,20 +3,38 @@
 class Driver():
     def __init__(self):
         self.run = True
-        self.board = [[i for i in file.read(9)] for i in range(9)] # of the form:   board[row][col]
+        self.board = [[int(i) for i in file.read(9)] for i in range(9)] # of the form:   board[row][col]
+        self.invalid = False
 
     def Run(self):
         self.CheckViableBoard()
         self.GeneralAnalysis()
-        while self.run:
-            self.FillInTiles()
-            self.RemainderAnalysis()
-            self.PairAnalysis()
+        self.FillInTiles()
+        self.RemainderAnalysis()
+        self.PairAnalysis()
+        if self.invalid:
+            print("This board is invalid")
 
     def CheckViableBoard(self):
+        # iterate thru every tile
         for row in range(len(self.board)):
             for col in range(len(self.board)):
-                print(self.board[row][col])
+                if self.board[row][col] != 0:
+                    # test the row that the tile is in, for every tile
+                    for i in range(len(self.board) - (col + 1)): # i ranges from 0 to the number of tiles left in the row
+                        testCol = i + col + 1 # testCol ranges from the index of the tile after the testTile to 8
+                        if self.board[row][testCol] == self.board[row][col]:
+                            self.invalid = True
+
+                    # test the column that the tile is in for every tile
+                    for i in range(len(self.board) - (row + 1)): # i ranges from 0 to the number of tiles left in the row
+                        testRow = i + row + 1
+                        print(self.board[testRow][col], end="")
+                        if self.board[testRow][col] == self.board[row][col]:
+                            self.invalid = True
+                    print("")
+
+                    # test the square that the tile is in, for every tile
 
     def GeneralAnalysis(self):
         pass
@@ -42,6 +60,3 @@ driver.Run()
 
 # close txt file
 file.close()
-
-
-
