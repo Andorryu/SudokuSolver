989223e6f3e8041f4ffc98518bfee2e26273fdf6
Andorryu <adority121@gmail.com>
2022-05-19 23:10:06 -0500

    Finished the GeneralAnalysis function
    Finished the GeneralAnalysis function, which looks at each empty space and determines what values are possible and which are impossible for that space.  The function results in updating the Driver class's impossibleNotes and possibleNotes 3-D lists to reflect this. The analysis steps are printed to the screen for debug and code-tracing purposes.

--- Sudoku Solver/Sudoku Solver/Sudoku_Solver.py
+++ Sudoku Solver/Sudoku Solver/Sudoku_Solver.py
@@ -4,21 +4,26 @@ class Driver():
     def __init__(self):
         self.run = True
         self.board = [[int(i) for i in file.read(9)] for i in range(9)] # of the form:   board[row][col]
+        self.impossibleNotes = [[[] for i in range(9)] for i in range(9)]
+        self.possibleNotes = [[[] for i in range(9)] for i in range(9)]
         self.invalid = False
         self.squareSize = int(math.sqrt(len(self.board)))
 
     def Run(self):
         self.CheckViableBoard()
-        self.GeneralAnalysis()
-        self.FillInTiles()
-        self.RemainderAnalysis()
-        self.PairAnalysis()
+        print("")
         if self.invalid:
             print("This board is INVALID")
         else:
             print("This board is VALID")
+        self.GeneralAnalysis()
+        self.FillInTiles()
+        self.RemainderAnalysis()
+        self.PairAnalysis()
 
     def CheckViableBoard(self):
+        print("Checking Viability of the board by checking if there are repeats in the rows, columns, or 3x3 squares")
+        print("")
         # iterate thru every tile
         for row in range(len(self.board)):
             for col in range(len(self.board)):
@@ -64,7 +69,77 @@ class Driver():
 
 
     def GeneralAnalysis(self):
-        pass
+        print("")
+        print("")
+        print("Noting which values are impossible for a given tile")
+        # iterate thru every tile
+        for row in range(len(self.board)):
+            for col in range(len(self.board)):
+                if self.board[row][col] == 0: # only work on the empty tiles
+
+                    print("")
+                    print("Testing the row of (", row, ",", col, ")")
+                    # populate impossibleNotes with impossible values based on the filled-in tiles in the row
+                    for testCol in range(len(self.board)):
+                        if self.board[row][testCol] != 0:
+                            if not self.ItemExistsInList(self.board[row][testCol], self.impossibleNotes[row][col]):
+                                self.impossibleNotes[row][col].append(self.board[row][testCol])
+                                print("Appended ", self.board[row][testCol], " to board notes")
+                            else:
+                                print("Skipping ", self.board[row][testCol], "...")
+                        # trace debug
+                        if testCol == 8:
+                            print("Board Notes:")
+                            print(self.impossibleNotes[row][col])
+
+                    print("Testing with columns now...")
+                    # populate impossibleNotes with impossible values based on the filled-in tiles in the column
+                    for testRow in range(len(self.board)):
+                        if self.board[testRow][col] != 0:
+                            if not self.ItemExistsInList(self.board[testRow][col], self.impossibleNotes[row][col]):
+                                self.impossibleNotes[row][col].append(self.board[testRow][col])
+                                print("Appended ", self.board[testRow][col], " to board notes")
+                            else:
+                                print("Skipping ", self.board[testRow][col], "...")
+                        # trace debug
+                        if testRow == 8:
+                            print("Board Notes:")
+                            print(self.impossibleNotes[row][col])
+                            
+                    print("Testing with 3x3 squares now...")
+                    # populate impossibleNotes with impossible values based on the filled-in tiles in the 3x3 square
+                    # get coords of each 3x3 square
+                    squareRow = math.floor(row/self.squareSize)
+                    squareCol = math.floor(col/self.squareSize)
+                    for i in range(self.squareSize):
+                        for j in range(self.squareSize):
+                            testRow = i + (squareRow * self.squareSize) # testRow and testCol cover every coord in the square that the testTile is in
+                            testCol = j + (squareCol * self.squareSize)
+                            if testRow != row and testCol != col: # dont test in the same row or column
+                                if self.board[testRow][testCol] != 0: # dont test it if it is blank
+                                    if not self.ItemExistsInList(self.board[testRow][testCol], self.impossibleNotes[row][col]):
+                                        self.impossibleNotes[row][col].append(self.board[testRow][testCol])
+                                        print("Appended ", self.board[testRow][testCol], " to board notes")
+                                    else:
+                                        print("Skipping ", self.board[testRow][testCol], "...")
+                            # trace debug
+                            if i == 2 and j == 2:
+                                print("Board Notes:")
+                                print(self.impossibleNotes[row][col])
+                    
+                    # subtract the list of impossible values from the list of every value from 1 to 9 to get all the possible values for that tile
+                    self.possibleNotes[row][col] = self.ListSubtraction([i+1 for i in range(9)], self.impossibleNotes[row][col])
+
+
+
+
+        # maybe the ratio of deduced possible values to deduced impossible values at this step determines the difficulty level of the puzzle?
+        print("")
+        print("The following lists the impossible values for each blank tile, read from left tile to right tile, and from top row to bottom row.")
+        print(self.impossibleNotes)
+        print("")
+        print("The following lists the possible values.")
+        print(self.possibleNotes)
 
     def FillInTiles(self):
         pass
@@ -74,6 +149,20 @@ class Driver():
 
     def PairAnalysis(self):
         pass
+
+    def ItemExistsInList(self, item, list):
+        for i in list:
+            if i == item:
+                return True
+        return False
+
+    def ListSubtraction(self, subtractor, subtractee): # assume that every value in subtractee is also in subtractor
+        subtraction = []
+        for i in subtractor:
+            if not self.ItemExistsInList(i, subtractee):
+                subtraction.append(i)
+        return subtraction
+
 
 
 # open txt file
